a5a7913d6a2bce45c9a55e37bffd4bb51d4e421d
SONU17 <sonu.lad2509@gmail.com>
2025-09-11 14:02:55 +0530

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 // base URL for your backend
 const API = axios.create({
-baseURL: "https://localhost:7098/api"
-//baseURL: process.env.REACT_APP_API_URL ,
+//baseURL: "https://localhost:7098/api"
+baseURL: process.env.REACT_APP_API_URL ,
 //baseURL: "https://vks-lms-api-cfaubjg4emfme8f4.centralindia-01.azurewebsites.net/api"
 });
-//console.log("Loaded API URL:", process.env.REACT_APP_API_URL);
+
 // ✅ Interceptor: attach token to every request
 API.interceptors.request.use(
   (config) => {
